6e3854d8d47984516a371f087838feda33d15a41
nzkks <kkshanthosh@gmail.com>
2024-07-14 11:28:15 +1200

    Create Navbar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <main className="bg-black">
+        <Navbar /> Hero Highlights
+      </main>
     </>
   );
 };
