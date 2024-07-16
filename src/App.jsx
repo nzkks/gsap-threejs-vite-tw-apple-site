793c7060957534b001c8492c4c8e8c9f3a861bae
nzkks <kkshanthosh@gmail.com>
2024-07-16 12:58:36 +1200

    Add Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 };
